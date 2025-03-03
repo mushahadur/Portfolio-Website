71f2f87d07597928030fe1571a05b55b2068ced5
mushahadur <mushahadurrahamankhanpulock@gmail.com>
2025-03-03 10:53:56 +0600

    first edition

--- assets/js/meyawo.js
+++ assets/js/meyawo.js
@@ -1,19 +1,5 @@
-/*!
-=========================================================
-* Meyawo Landing page
-=========================================================
-
-* Copyright: 2019 DevCRUD (https://devcrud.com)
-* Licensed: (https://devcrud.com/licenses)
-* Coded by www.devcrud.com
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
 // smooth scroll
-$(document).ready(function(){
+$(document).ready(function() {
     $(".navbar .nav-link").on('click', function(event) {
 
         if (this.hash !== "") {
@@ -24,15 +10,15 @@ $(document).ready(function(){
 
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
-            }, 700, function(){
+            }, 700, function() {
                 window.location.hash = hash;
             });
-        } 
+        }
     });
 });
 
 // navbar toggle
-$('#nav-toggle').click(function(){
+$('#nav-toggle').click(function() {
     $(this).toggleClass('is-active')
     $('ul.nav').toggleClass('show');
 });
